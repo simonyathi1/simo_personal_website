4fd6fca5c61efe6b7d01664b6ce940e2ce21b7ef
Onesimo Nyathi <simonyathi1@gmail.com>
2024-05-18 19:18:00 +0200

    update git actions and workflows

--- lib/modules/home/widgets/navigation/site_footer/footer_widget.dart
+++ lib/modules/home/widgets/navigation/site_footer/footer_widget.dart
@@ -8,23 +8,30 @@ class FooterWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(18),
-      height: 100,
-      color: lightSecondaryColor,
-      child: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text("© 2024. Onesimo Nyathi, by FideliTech", style: TextStyle(color: lightBackgroundColor),),
-          Column(
+    return Column(
+      children: [ const Padding(
+        padding: EdgeInsets.only(bottom: 20.0, top: 20.0),
+        child: Divider(height: 1,thickness: 0.5, color: Colors.black),
+      ),
+        Container(
+          padding: const EdgeInsets.all(18),
+          height: 100,
+          //color: lightSecondaryColor,
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("simonyathi1@gmail.com", style: TextStyle(color: lightBackgroundColor),),
-              Text("onesimonyathi@fidelitech.co.za", style: TextStyle(color: lightBackgroundColor),),
+              Text("© 2024. Onesimo Nyathi, by FideliTech", style: TextStyle(color: lightSecondaryColor, fontFamily: 'Montserrat'),),
+              Column(
+                children: [
+                  Text("simonyathi1@gmail.com", style: TextStyle(color: lightSecondaryColor, fontFamily: 'Montserrat'),),
+                  Text("onesimonyathi@fidelitech.co.za", style: TextStyle(color: lightSecondaryColor, fontFamily: 'Montserrat'),),
+                ],
+              ),
+              SocialSectionWidget(originalColor: lightSecondaryColor,)
             ],
           ),
-          SocialSectionWidget(originalColor: lightBackgroundColor,)
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
